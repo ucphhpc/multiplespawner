a5ccfab2f0bc17f3d8a188bc8c6888b11684953b
Rasmus Munk <munk1@live.dk>
2020-09-14 08:54:53 +0200

    Adjust the jupyterhub example configuration

--- examples/jupyterhub_config.py
+++ examples/jupyterhub_config.py
@@ -1,14 +1,18 @@
+import os
 from jupyter_client.localinterfaces import public_ips
 
 c = get_config()
 
-c.JupyterHub.ip = "127.0.0.1"
+c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.port = 8000
 
-c.JupyterHub.spawner_class = "multiplespawner.MultipleSpawner"
+# c.JupyterHub.spawner_class = "multiplespawner.MultipleSpawner"
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.hub_ip = public_ips()[0]
-c.JupyterHub.hub_ip_connect = public_ips()[0]
-c.JupyterHub.port = 8080
 
+# c.DockerSpawmer.use_internal_ip = True
+c.DockerSpawner.hub_ip_connect = public_ips()[0]
+c.DockerSpawner.image = 'nielsbohr/python-notebook'
 
 c.JupyterHub.authenticator_class = "jhubauthenticators.DummyAuthenticator"
 c.DummyAuthenticator.password = "password"
